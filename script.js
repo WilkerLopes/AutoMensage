aadc5f5e661d32f731b1b483f7ce6b545149c591
Wilker Lopes Cruz <wilkerlopes257@gmail.com>
2020-05-19 04:39:36 -0400

    Alterando a impressão de horários

--- script.js
+++ script.js
@@ -1,112 +1,112 @@
 var numCll = mensage = validation = "";
 var ligar = false;
 var toggleFav = localStorage.getItem("tgFav");
-if(toggleFav == null){
+if (toggleFav == null) {
     toggleFav = false;
-  }else{
+} else {
     toggleFav = true;
-  }
-  console.log(toggleFav);
- if(toggleFav == true){
+}
+console.log(toggleFav);
+if (toggleFav == true) {
     $("#toggleFavorite").html("<i class='material-icons'>star</i>");
-  }else{
-     $("#toggleFavorite").html("<i class='material-icons'>star_border</i>");
-  }
+} else {
+    $("#toggleFavorite").html("<i class='material-icons'>star_border</i>");
+}
 
 $(document).ready(() => {
-  
 
- 
 
-  $("#msgPreview").hide();
-  $('.celular').mask('0 0000-0000', { placeholder: "9 1234-1234" });
 
-  $("#telefone").change(() => {
-    numCll = "" + $("#telefone").val();
-    if (numCll.length != 11) {
-      M.toast({ html: 'Numero incompleto!!' });
-    } else {
-      enviarMsg();
-      ligar = true;
-    };
-  });
 
-  $("#telefone").keyup(() => {
-    numCll = "" + $("#telefone").val();
-    $("#telefonePreview").text("+55 92 " + numCll);
-  });
+    $("#msgPreview").hide();
+    $('.celular').mask('0 0000-0000', { placeholder: "9 1234-1234" });
 
-  $("#msg").change(() => {
-    mensage = $("#msg").val();
-    if (mensage == null || mensage == "" || mensage == " " || mensage == "  " || mensage == "   " || mensage == "    ") {
-      M.toast({ html: 'Nenhuma mensagem definida!!' });
-      $("#msgPreview").hide();
-    };
-    enviarMsg();
-  });
+    $("#telefone").change(() => {
+        numCll = "" + $("#telefone").val();
+        if (numCll.length != 11) {
+            M.toast({ html: 'Numero incompleto!!' });
+        } else {
+            enviarMsg();
+            ligar = true;
+        };
+    });
 
-  $("#msg").keyup(() => {
-    mensage = "" + $("#msg").val();
-    $("#msgPreview").show().text(mensage);
-    enviarMsg();
-  });
+    $("#telefone").keyup(() => {
+        numCll = "" + $("#telefone").val();
+        $("#telefonePreview").text("+55 92 " + numCll);
+    });
 
-  $("#ligar").click(()=>{
-  if(ligar==false){
-     M.toast({ html: 'Digite um número para continuar'});
-  }
-});
+    $("#msg").change(() => {
+        mensage = $("#msg").val();
+        if (mensage == null || mensage == "" || mensage == " " || mensage == "  " || mensage == "   " || mensage == "    ") {
+            M.toast({ html: 'Nenhuma mensagem definida!!' });
+            $("#msgPreview").hide();
+        };
+        enviarMsg();
+    });
 
-$("#toggleFavorite").click(()=>{
-  if(toggleFav == true){
-    $("#toggleFavorite").html("<i class='material-icons'>star_border</i>");
-    localStorage.setItem("tgFav", false);
-  }else{
-     $("#toggleFavorite").html("<i class='material-icons'>star</i>");
-     localStorage.setItem("tgFav", true);
-  }
-  toggleFav = localStorage.getItem("tgFav");
-});
+    $("#msg").keyup(() => {
+        mensage = "" + $("#msg").val();
+        $("#msgPreview").show().text(mensage);
+        enviarMsg();
+    });
 
-$(".preloader").hide();
+    $("#ligar").click(() => {
+        if (ligar == false) {
+            M.toast({ html: 'Digite um número para continuar' });
+        }
+    });
+
+    $("#toggleFavorite").click(() => {
+        if (toggleFav == true) {
+            $("#toggleFavorite").html("<i class='material-icons'>star_border</i>");
+            localStorage.setItem("tgFav", false);
+        } else {
+            $("#toggleFavorite").html("<i class='material-icons'>star</i>");
+            localStorage.setItem("tgFav", true);
+        }
+        toggleFav = localStorage.getItem("tgFav");
+    });
+
+    $(".preloader").hide();
 });
 
 function enviarMsg() {
-  var link = "https://api.whatsapp.com/send?phone=5592" + numCll + "&text=" + mensage + " ";
-  var call = "tel:5592" + numCll;
-  $("#enviarMsg").attr("href", link);
-  $("#ligar").attr("href", call);
+    var link = "https://api.whatsapp.com/send?phone=5592" + numCll + "&text=" + mensage + " ";
+    var call = "tel:5592" + numCll;
+    $("#enviarMsg").attr("href", link);
+    $("#ligar").attr("href", call);
 }
 
 function proximo(numN) {
-  if(numCll.length == 11){
-    var numberCalc = numCll.substring(2,11);
-    var numberCalc = numberCalc.split("-");
-    if(numN > 0){
-      if(numberCalc[1]==9999){
-        numberCalc[0]++;
-        numberCalc[1]=0000;
-      }else{
-        numberCalc[1]++;
-      }
-    }else{
-      if(numberCalc[1]==9999){
-        numberCalc[0]--;
-        numberCalc[1]=0000;
-      }else{
-        numberCalc[1]--;
-      }
+    if (numCll.length == 11) {
+        var numberCalc = numCll.substring(2, 11);
+        var numberCalc = numberCalc.split("-");
+        if (numN > 0) {
+            if (numberCalc[1] == 9999) {
+                numberCalc[0]++;
+                numberCalc[1] = 0000;
+            } else {
+                numberCalc[1]++;
+            }
+        } else {
+            if (numberCalc[1] == 9999) {
+                numberCalc[0]--;
+                numberCalc[1] = 0000;
+            } else {
+                numberCalc[1]--;
+            }
+        }
+        numCll = "9 " + numberCalc[0] + "-" + numberCalc[1];
+        $("#telefone").val(numCll);
+        enviarMsg();
+    } else {
+        M.toast({ html: 'Digite um número para continuar' });
     }
-    numCll = "9 "+numberCalc[0]+"-"+numberCalc[1];
-    $("#telefone").val(numCll);
-    enviarMsg();
-  }else{
-     M.toast({ html: 'Digite um número para continuar'});
-  }
 }
 
 
-function relogio(){
+function relogio() {
     hrAtual = new Date();
     hora = hrAtual.getHours();
     minuto = hrAtual.getMinutes();
@@ -114,7 +114,7 @@ function relogio(){
 
     horaImprimivel = hora + " : " + minuto;
 
-    $(".hrPreview").text(horaImprimivel);
+    $(".time").text(horaImprimivel);
 
     requestAnimationFrame(relogio);
 }
